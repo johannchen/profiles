412a0680ad4f19331aa3e57e89ff28375d870c98
Tim Morgan <tim@timmorgan.org>
2011-12-01 17:28:36 -0600

    Improve message specs.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,15 +1,11 @@
 require 'spec_helper'
 
 describe Message do
-  describe '#profile=' do
+  describe '#set_method' do
     context 'given a facebook recipient' do
       before do
         @profile = Factory.create(:profile, :facebook_id => '1234')
         @message = Factory.build(:message, :profile => @profile)
-      end
-
-      it 'sets the profile_id' do
-        @message.profile_id.should eq(@profile.id)
       end
 
       it 'sets the method to "mailto"' do
@@ -33,30 +29,73 @@ describe Message do
         @message = Factory.build(:message, :profile => @profile)
       end
 
-      it 'sets the profile_id' do
-        @message.profile_id.should eq(@profile.id)
-      end
-
       it 'sets the method to "mailto"' do
         @message.method.should eq('mailto')
       end
     end
-  end
 
-  describe '#profile_id=' do
-    before do
-      @profile = Factory.create(:profile, :facebook_id => '1234')
-      Profile.stubs(:find).returns(@profile)
-      @message = Factory.build(:message, :profile_id => @profile.id)
-    end
+    describe 'triggers' do
+      before do
+        @profile = Factory.create(:profile)
+        @message = Factory.build(:message)
+        @message.stub(:set_method)
+      end
 
-    # FIXME mocha/bourne have_received isn't working for some reason; punting for now
-    #it 'calls Profile.find(id)' do
-      #Profile.should have_received(:find).with(@profile.id)
-    #end
+      describe '#profile=' do
+        before do
+          @message.profile = @profile
+        end
 
-    it 'sets the profile_id' do
-      @message.profile_id.should eq(@profile.id)
+        it 'calls set_method' do
+          @message.should have_received(:set_method)
+        end
+
+        it 'sets the profile_id' do
+          @message.profile_id.should eq(@profile.id)
+        end
+      end
+
+      describe '#profile_id=' do
+        before do
+          @message.profile_id = @profile.id
+        end
+
+        it 'calls set_method' do
+          @message.should have_received(:set_method)
+        end
+
+        it 'sets the profile_id' do
+          @message.profile_id.should eq(@profile.id)
+        end
+      end
+
+      describe '#from=' do
+        before do
+          @message.from = @profile
+        end
+
+        it 'calls set_method' do
+          @message.should have_received(:set_method)
+        end
+
+        it 'sets the from_id' do
+          @message.from_id.should eq(@profile.id)
+        end
+      end
+
+      describe '#from_id=' do
+        before do
+          @message.from_id = @profile.id
+        end
+
+        it 'calls set_method' do
+          @message.should have_received(:set_method)
+        end
+
+        it 'sets the from_id' do
+          @message.from_id.should eq(@profile.id)
+        end
+      end
     end
   end
 end

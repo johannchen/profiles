43482c3710182ec4cd528eb784e6442dc61e5e83
Tim Morgan <tim@timmorgan.org>
2011-12-01 13:23:21 -0600

    Helper for check boxes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -82,4 +82,23 @@ module ApplicationHelper
     end
   end
 
+  def check_box_fields(label, fields)
+    content_tag(:div, :class => 'clearfix') do
+      label_tag(fields.first[:name], label) +
+      content_tag(:div, :class => 'input') do
+        content_tag(:ul, :class => 'inputs-list') do
+          fields.map do |field|
+            content_tag(:li) do
+              (field[:value] == true ? hidden_field_tag(field[:name], false, :id => nil) : '') +
+              check_box_tag(field[:name], field[:value], field[:checked])
+            end
+          end.join.html_safe
+        end
+      end
+    end
+  end
+
+  def check_box_field(label, name, value, checked)
+    check_box_fields(label, [{:name => name, :value => value, :checked => checked}])
+  end
 end

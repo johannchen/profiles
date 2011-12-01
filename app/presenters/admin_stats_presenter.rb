f4600b6916bbccd5e8e05dd7f44d147ffdb20855
Tim Morgan <tim@timmorgan.org>
2011-11-30 23:06:05 -0600

    Add more stats to admin dashboard [finishes #26]

--- app/presenters/admin_stats_presenter.rb
+++ app/presenters/admin_stats_presenter.rb
@@ -8,6 +8,13 @@ class AdminStatsPresenter
     records_by_date(records, :created_at, date - days.days, date)
   end
 
+  def messages_by_date(days=30)
+    days -= 1
+    date = Time.zone.now
+    records = Message.where(['created_at >= ?', Time.now - days.days])
+    records_by_date(records, :created_at, date - days.days, date)
+  end
+
   private
 
   def records_by_date(records, date_method, start_date, stop_date)
